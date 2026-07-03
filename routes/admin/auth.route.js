b507a0782cc68a56bc39e502fb68e32a3404fbb0
minhvietyp <minhvietyp@gmail.com>
2026-07-03 17:19:24 +0700

    add authorization for each page(with role.permissions.include)

---
+++ routes/admin/auth.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const route = express.Router();
+const controller = require('../../controllers/admin/auth.controller');
+const validate = require('../../validate/admin/auth.validate');
+
+route.get('/login', controller.login);
+
+route.post('/login', validate.loginPost, controller.loginPost);
+
+route.get('/logout', controller.logout);
+
+
+
+module.exports = route;
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,15 +4,38 @@ const productRoute = require('./product.route');
 const productCategoryRoute = require('./product-catagory.route');
 const roleRoute = require('./role.route');
 const accountRoute = require('./account.route');
+const authRoute = require('./auth.route');
+const middlewareAuth = require('./../../middlewares/admin/auth.middleware')
 
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
-    app.use(PATH_ADMIN + "/dashboard", dashboardRoute);
-    app.use(PATH_ADMIN + "/products", productRoute);
-    app.use(PATH_ADMIN + "/product-category", productCategoryRoute);
-    app.use(PATH_ADMIN + "/roles", roleRoute);
-    app.use(PATH_ADMIN + "/accounts", accountRoute);
+    app.use(
+        PATH_ADMIN + "/dashboard",
+        middlewareAuth.requireAuth,
+        dashboardRoute
+    );
+    app.use(
+        PATH_ADMIN + "/products",
+        middlewareAuth.requireAuth,
+        productRoute
+    );
+    app.use(
+        PATH_ADMIN + "/product-category",
+        middlewareAuth.requireAuth,
+        productCategoryRoute
+    );
+    app.use(
+        PATH_ADMIN + "/roles",
+        middlewareAuth.requireAuth,
+        roleRoute
+    );
+    app.use(
+        PATH_ADMIN + "/accounts",
+        middlewareAuth.requireAuth,
+        accountRoute
+    );
+    app.use(PATH_ADMIN + "/auth", authRoute);
 
 }
